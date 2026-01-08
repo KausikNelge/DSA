32ca83d5927c461a6b91cf202cb18581b26e3ca6
kausik nelge <1ew23ic019@ewit.edu.in>
2026-01-08 21:16:22 +0530

    revision ka bhi revision

---
+++ sll revision1.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self,info,next=None):
+        self.data = info
+        self.next = next
+
+class SLL:
+    def __init__(self,head=None):
+        self.head = head
+
+    def InsertAtEnd(self,value):
+        temp = Node(value)
+        if (self.head != None):
+            t1 = self.head
+            while (t1.next != None):
+                 t1 = t1.next
+            t1.next = temp 
+        else:
+            self.head = temp
+
+    def printLL(self):
+        t1= self.head
+        while t1 is not None:
+             print(t1.data)
+             t1=t1.next
+
+
+obj=SLL()
+obj.InsertAtEnd(10)
+obj.InsertAtEnd(20)
+obj
+
+
+obj.printLL()
--- sll.py
+++ sll.py
@@ -1,1 +1,13 @@
 # learning sll in dsa by chai aur code
+
+class Node:
+    def  __init__(self,info,next=None):
+        self.data= info
+        self.next = next
+
+class SinglyLinkedlist:
+    def __init__(self,head=None):
+        self.head = head
+
+
+    def inserstAtEnd(self,value)
